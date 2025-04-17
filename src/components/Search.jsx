86a31084656a00e49c4604b7dea3979f7ca6c31a
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-17 01:45:19 -0400

    Added styling and deleted default stylesheet

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,7 +5,6 @@ function Search({search, setSearch, handleSearch}) {
         <div className="search-engine">
             <input 
                 type="text" 
-                className="city-search" 
                 placeholder="Enter City Name" 
                 name="search" 
                 value={search} 
