2ad21685a6eb3de4c662aab944c071abe3210443
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-17 01:38:50 -0400

    Added search input box

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,16 @@
 import '../css/Search.css';
 
-function Search() {
+function Search({search, setSearch}) {
     return (
-        <div>
-
+        <div className="search-engine">
+            <input 
+                type="text" 
+                className="city-search" 
+                placeholder="Enter City Name" 
+                name="search" 
+                value={search} 
+                onChange={(event) => setSearch(event.target.value)}
+            />
         </div>
     )
 }
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,10 @@
 import '../css/Weather.css';
+import Search from '../components/Search';
 
 function Weather() {
     return (
         <div>
+            <Search />
             Weather
         </div>
     )
