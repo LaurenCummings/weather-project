25d8ed4d585b04f5a9c569e8b02116468d0768ee
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-18 02:45:36 -0400

    Added api call for weather data

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,36 @@
 import '../css/Weather.css';
+import { useState } from 'react';
 import Search from '../components/Search';
 
 function Weather() {
+    const [search, setSearch] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [weatherData, setWeatherData] = useState(null);
+
+    async function fetchWeatherData(param) {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=e34b4c51d8c2b7bf48d5217fe52ff79e`);
+
+            const data = await response.json();
+
+            console.log(data, 'data');
+
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    function handleSearch() {
+        fetchWeatherData(search);
+    }
+
     return (
         <div>
-            <Search />
+            <Search 
+                search={search}
+                setSearch={setSearch}
+                handleSearch={handleSearch}
+            />
             Weather
         </div>
     )
